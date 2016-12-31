a69a1d928cbc62e60025416ac9193e54d71f0145
Ch1b1k0 <baileykanoe@hawaii.rr.com>
2016-12-31 11:52:13 -1000

    almost done with js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
-function getBackgroundColor(color){
-
+function getBackgroundColor(idName){
+	var element = document.getElementById(idName);
+	var style = window.getComputedStyle(element,null).getPropertyValue("background-color")
+	document.getElementById("selectedColorName").style.backgroundColor = style
 }
 
-function changeColorOnClick(canvasStuff){
-	
+function changeColorOnClick(idName){
+	var orignalElementColor = document.getElementById("selectedColorName").style.backgroundColor
+	document.getElementById(idName).style.backgroundColor = orignalElementColor
+	console.log(orignalElementColor)
 }
